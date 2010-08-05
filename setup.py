387c21d0d882b33e33ce80c19ee228eb7d94e36a
Orne Brocaar <info@brocaar.com>
2010-08-05 15:22:41 +0200

    New code added. Old code removed.

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from distutils.core import setup
 
-setup(name='google_analytics',
-      version='0.2',
-      description='A simple Django application to integrate Google Analytics into your projects',
-      author='Clint Ecker',
-      author_email='me@clintecker.com',
-      url='http://github.com/clintecker/django-google-analytics/tree/master',
-      packages=['google_analytics','google_analytics.templatetags',],
-      package_data={'google_analytics': ['templates/google_analytics/*.html']},
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-      )
+
+setup(
+    name='Django Google Tools',
+    version='0.1',
+    description='A simple Django app for managing Google Analytics and Site Verification codes.',
+    author='Orne Brocaar',
+    author_email='info@brocaar.com',
+    url='http://github.com/LUKKIEN/django-google-tools',
+    packages=[
+        'googletools',
+        'googletools.migrations',
+        'googletools.templatetags',
+    ]
+)
